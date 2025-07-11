bdedd723da75db14318722db06fa8b01eab80496
Taras Protsyshyn <protsyshyn.t@gmail.com>
2025-07-11 17:19:57 +0000

    init project

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, TypeScript !!!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
