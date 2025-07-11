1e4b689cde05a2144c50f9b9b80577988ce68058
Taras Protsyshyn <protsyshyn.t@gmail.com>
2025-07-11 19:01:27 +0000

    add linting

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,12 @@
 import express, { Request, Response } from "express";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ?? 3000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, TypeScript !!!");
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port.toString()}`);
 });
