c61b87bfc550a2c23a833fda3633473289e56333
Taras Protsyshyn <protsyshyn.t@gmail.com>
2025-08-28 18:30:34 +0000

    add config for db

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response } from "express";
 
+import { connectToDatabase } from "./config/db.js";
+
 const app = express();
 const port = process.env.PORT ?? 3000;
+
+await connectToDatabase();
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, TypeScript !!!");
